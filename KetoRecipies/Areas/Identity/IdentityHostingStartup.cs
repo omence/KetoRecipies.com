75256a6cecd5cd0f91e2d1d1655a58bd5b510847
Jason Few <omence11@gmail.com>
2019-07-18 10:35:16 -0700

    Added Identity roles Admin and Member

--- KetoRecipies/Areas/Identity/IdentityHostingStartup.cs
+++ KetoRecipies/Areas/Identity/IdentityHostingStartup.cs
@@ -3,6 +3,7 @@ using KetoRecipies.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,8 +20,29 @@ namespace KetoRecipies.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("KetoRecipiesContextConnection")));
 
-                services.AddDefaultIdentity<ApplicationUser>()
-                    .AddEntityFrameworkStores<KetoRecipiesContext>();
+                services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<KetoRecipiesContext>()
+                    .AddDefaultTokenProviders();
+
+                services.AddMvc()
+        .AddRazorPagesOptions(options =>
+        {
+            options.AllowAreas = true;
+            options.Conventions.AuthorizeAreaFolder("Identity", "/Account/Manage");
+            options.Conventions.AuthorizeAreaPage("Identity", "/Account/Logout");
+            options.Conventions.AuthorizeAreaPage("Identity", "/Account/ForgotPassword");
+            options.Conventions.AuthorizeAreaPage("Identity", "/Account/ResetPassword");
+            options.Conventions.AuthorizeAreaPage("Identity", "/Account/ResetPasswordConfirmation");
+        });
+
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.LoginPath = $"/Identity/Account/Login";
+                    options.LogoutPath = $"/Identity/Account/Logout";
+                    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+                });
+                services.AddSingleton<IEmailSender, EmailSender>();
+
             });
         }
     }
---
+++ KetoRecipies/Models/RoleInitializer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KetoRecipies.Models
+{
+    public class RoleInitializer
+    {
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+        {
+            new IdentityRole{Name = ApplicationRoles.Member, NormalizedName = ApplicationRoles.Member.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() },
+            new IdentityRole{Name = ApplicationRoles.Admin, NormalizedName = ApplicationRoles.Admin.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() }
+        };
+
+        /// <summary>
+        /// Adds any existing roles to the database
+        /// </summary>
+        /// <param name="context">ApplicationDbContext database context</param>
+        private static void AddRoles(KetoRecipiesContext context)
+        {
+            if (context.Roles.Any()) return;
+
+            foreach (var role in Roles)
+            {
+                context.Roles.Add(role);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Seeds data for existing roles
+        /// </summary>
+        /// <param name="serviceProvider">Service provider interview</param>
+        public static void SeedData(IServiceProvider serviceProvider)
+        {
+            using (var dbContext = new KetoRecipiesContext(serviceProvider.GetRequiredService<DbContextOptions<KetoRecipiesContext>>()))
+            {
+                dbContext.Database.EnsureCreated();
+                AddRoles(dbContext);
+            }
+        }
+    }
+}
